a64fa03c1474c3f00790a74cc0e41c83d8365be6
Nelson Segarra <junicus@outlook.com>
2016-03-06 04:52:42 -0400

    first impletmetation of challenge

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+app.get('/', function (req, res) {
+  var osregex = new RegExp(/\(([^\)]+)\)/g)
+  var data = {
+    ipaddress: req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress,
+    language: req.headers['accept-language'].split(',')[0],
+    software: osregex.exec(req.headers['user-agent'])[1]
+  }
+
+  console.log(req.headers)
+  res.json(data)
+})
+
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'))
+})
