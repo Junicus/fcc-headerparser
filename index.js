3702dfcd3d80089a6b5a331062f2363f39eca3da
Nelson Segarra <junicus@outlook.com>
2016-03-07 20:16:52 -0400

    linting enabled, added .jscsrc and fixed coding style

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 
-app.get('/', function (req, res) {
-  var osregex = new RegExp(/\(([^\)]+)\)/g)
-  var data = {
-    ipaddress: req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress,
-    language: req.headers['accept-language'].split(',')[0],
-    software: osregex.exec(req.headers['user-agent'])[1]
-  }
+app.get('/', function(req, res) {
+    var osregex = new RegExp(/\(([^\)]+)\)/g)
+    var data = {
+        ipaddress: req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress || req.connection.socket.remoteAddress,
+        language: req.headers['accept-language'].split(',')[0],
+        software: osregex.exec(req.headers['user-agent'])[1]
+    }
 
-  console.log(req.headers)
-  res.json(data)
+    console.log(req.headers)
+    res.json(data)
 })
 
-app.listen(app.get('port'), function () {
-  console.log('Node app is running on port', app.get('port'))
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'))
 })
